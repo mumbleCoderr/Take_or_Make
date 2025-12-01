51a2f603922611f1db475840e361807504de53ef
pragma once <mat02biernat@gmail.com>
2025-12-01 14:19:47 +0100

    bottom bar elements

--- app/src/main/java/com/biernatmdev/simple_service/features/nav/Screen.kt
+++ app/src/main/java/com/biernatmdev/simple_service/features/nav/Screen.kt
@@ -4,14 +4,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class Screen {
-
     @Serializable
     data object SplashScreen: Screen()
-
     @Serializable
     data object AuthScreen: Screen()
-
     @Serializable
     data object HomeGraph: Screen()
-
+    @Serializable
+    data object HomeScreen: Screen()
+    @Serializable
+    data object CategoryScreen: Screen()
+    @Serializable
+    data object NotificationScreen: Screen()
+    @Serializable
+    data object ProfileScreen: Screen()
 }
