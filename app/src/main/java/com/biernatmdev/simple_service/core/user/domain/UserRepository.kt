3001351ae1534c01cb9f8f8a2816151a5d83108f
pragma once <mat02biernat@gmail.com>
2025-12-05 16:02:05 +0100

    signout method added to userrepo

--- app/src/main/java/com/biernatmdev/simple_service/core/user/domain/UserRepository.kt
+++ app/src/main/java/com/biernatmdev/simple_service/core/user/domain/UserRepository.kt
@@ -5,4 +5,5 @@ import com.google.firebase.auth.FirebaseUser
 interface UserRepository {
     fun getCurrentUserId(): String?
     suspend fun createUser(user: FirebaseUser): Result<Unit>
+    suspend fun signOut(): Result<Unit>
 }
